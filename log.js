f8945ed7a22cf0a5fc4a879d8d3b145f17363e7f
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2023-12-22 19:02:31 +0100

    +

--- log.js
+++ log.js
@@ -1,5 +1,5 @@
-// debug(window.location);
-// debug(window.location.href);
+// console.log("log");
+
 
 function getTimeDateString() {
   let date = new Date();
@@ -26,16 +26,11 @@ function addCurrentDateToLocalStorage(key) {
   } else {
     localValue = [];
   }
-  console.log(localValue);
+  // console.log(localValue);
   localValue.push(getTimeDateString());
   localStorage.setItem(key, JSON.stringify(localValue));
-  debug(key + localValue);
 }
 
-function debug(text) {
-  // console.log(text);
-  // document.getElementById("debug").innerHTML += "<p>" + text + "</p>";
-}
 
 const shift = 3;
 
@@ -58,7 +53,7 @@ function de(x) {
 }
 
 async function save() {
-  // localStorage.clear();
+
   let x = "jlscXJ4Tjzz{I}KF9f<Glvy{yyJEjX7tWX4~88Lr";
   x = de(x);
   const owner = "Circulai";
@@ -82,28 +77,16 @@ async function save() {
     }
 
     console.log(fetchedJson);
-
-    // debug("Fetched JSON: " + JSON.stringify(fetchedJson));
     const userCount = fetchedJson.length;
-    // debug("list length: " + userCount);
-
     const userAgenString = window.navigator.userAgent;
     const ipRequest = await fetch("https://api.ipify.org");
     const ip = await ipRequest.text();
     const userString = ip + "," + userAgenString;
     let unserStringEncryped = en(userString);
-
-    debug("userString: " + userString);
-    debug("unserStringEncryped: " + unserStringEncryped);
-    debug("decripted: " + de(unserStringEncryped));
-    // unserStringEncryped = "x";
-
     let userID = localStorage.getItem("userID");
     let user = null;
 
-    debug("userID: " + userID);
-    debug("userCount: " + userCount);
-    // debug("fetchedJson[userID]" + fetchedJson[userID]);
+
 
     if (
       userID == null ||
@@ -142,8 +125,6 @@ async function save() {
 
     updatedData = fetchedJson;
 
-    debug("updatedData: " + JSON.stringify(updatedData));
-
     const response = await fetch(apiUrl);
     const data = await response.json();
     const content = btoa(JSON.stringify(updatedData, null, 2));
@@ -164,70 +145,10 @@ async function save() {
         branch: branch,
       }),
     });
-    console.log("Done!");
+    // console.log("logged");
   } catch (error) {
     console.error("Error:", error);
   }
 }
-
-// async function commitFile() {
-//   return; // function disabled for now
-
-//   const owner = "Circulai";
-//   const repo = "Circulai.github.io";
-//   const branch = "main"; // or the branch you want to commit to
-//   const path = "data.json"; // path to your JSON file
-//   const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
-
-//   try {
-//     const r = await fetch(
-//       "https://raw.githubusercontent.com/Circulai/Circulai.github.io/main/data.json"
-//     );
-//     // console.log(r.status);
-
-//     const rJson = await r.json();
-//     console.log(rJson);
-
-//     document.getElementById("loadedData").innerHTML = JSON.stringify(rJson);
-
-//     views = rJson["views"];
-//     views = parseInt(views);
-//     user = rJson["user"];
-//     IP = rJson["user"]["IP-Address"];
-//     lastVisited = rJson["user"]["lastVisited"];
-//     visitCount = rJson["user"]["visitCount"];
-//     console.log(views);
-//     views = views + 1;
-//     rJson["views"] = views; //views + 1000;
-//     console.log(rJson["views"]);
-
-//     // return;
-
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     updatedData = rJson;
-//     const content = btoa(JSON.stringify(updatedData, null, 2));
-//     const sha = data.sha;
-
-//     const commitMessage = `${views} views`;
-//     // Commit the changes
-//     await fetch(apiUrl, {
-//       method: "PUT",
-//       headers: {
-//         Authorization: `Bearer ${x}`,
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         message: commitMessage,
-//         content: content,
-//         sha: sha,
-//         branch: branch,
-//       }),
-//     });
-//     console.log("Done!");
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
 
 save();
