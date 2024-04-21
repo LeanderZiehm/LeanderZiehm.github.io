73f86cc749c03dd111685069abc4b425d4b7ae5c
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2024-04-21 22:31:47 +0200

    added execieses till 12

--- exercises.js
+++ exercises.js
@@ -38,11 +38,10 @@ const exercises = [{
 },{
     task: 'Define an atom called number which as the elements 1 and 20. Generate an atom called biggerThan10 that uses a rule to copy the numbers that are bigger than 10. ',
     tip: 'Rules that only copy certain elements can be achieved through adding each condition with a , after the initial rule',
-    example: '',
+    example: 'meetsConditions(Number) :- number(Number), Number < 40, Number > 5, Number == 30.',
     solution: 'number(1;20). biggerThan10(Number) :- number(Number),Number>10.',
     output: 'number(1) number(20) biggerThan10(20)',
     description: 'introduction to rules that use conditions on numbers',
-    TODO: "EXERCISE 6!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 },
 
  {
@@ -52,32 +51,55 @@ const exercises = [{
     solution: 'person(avery,female). person(logan,male). person(casey,female). isFemale(Name) :- person(Name,female).',
     output: 'person(avery,female) person(logan,male) person(casey,female) isFemale(avery) isFemale(casey)',
     description: 'rules to copy subgroups of elements',
-    TODO: "[MAKE EXERCISE 7 CLEARER!]"
-}, {
-    task: 'Add another person called Ben. Ben likes every color. Create a rule that automatically assigns Ben every color in the color atom. Do not assign the colors manually!',
-    tip: '',
-    example: '',
-    solution: 'color(red;green;blue). favoriteColor(ben,Color) :- color(Color).',
-    output: 'color(red) color(green) color(blue)',
-    description: '',
-    TODO: "FIX EXERCISE 8!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-},{
-    task: '',
-    tip: '',
-    example: '',
-    solution: '.',
-    output: '',
-    description: ''
-} 
+    TODO: "(MAKE EXERCISE 7 CLEARER)"
+}, 
 
-,{
+{
     task: 'Create an atom called sports. It should contain football and badminton. Create a choice rule that instantiates a new atom called choice. It should choose whether you want to not take any sport, one of them, or both.',
     tip: '',
     example: '{ newAtom(Variable) : otherAtom(Variable) }.',
     solution: 'sport(football;badminton). { choice(S) : sport(S) }.',
     output: '[Models       : 4]\nAnswer: 1\nsport(football) sport(badminton)\nAnswer: 2\nsport(football) sport(badminton) choice(badminton)\nAnswer: 3\nsport(football) sport(badminton) choice(football)\nAnswer: 4\nsport(football) sport(badminton) choice(football) choice(badminton)',
-    description: ''
-} 
+    description: 'introduction to choice rules using single element.'
+},
+
+
+{
+    task: 'color(red;green;blue). Define a two-element atom where the first element is ben and the second element gets assigned by a choice rule. Assign ben all combinations of colors.',
+    tip: 'Choice rules uses {} around the new atom to create all combinations of elements using multiple models. You can think of them almost as paralell universes.',
+    example: '{newAtom(constant,Varable)} :- atom(Varable).',
+    solution: 'color(red;green;blue).{newAtom(ben,Color)} :- color(Color).',
+    output: '8 Models',
+    description: 'introduction to choice rules using one constant'
+},
+{
+    task: 'person(ben;avery).color(red;green). Use choice rules to assign all color combinations to all people.',
+    tip: 'You can add another atoms elements by adding : behind be new atom and then specifying the other atom with a variable.',
+    example: '{newAtom(Varable1,Varable2): atom1(Varable1)} :- atom2(Varable2).',
+    solution: 'person(ben;avery).color(red;green).{newAtom(Varable1,Varable2): person(Varable1)} :- color(Varable2).',
+    output: '64 Models',
+    description: 'introduction to choice rules using two varaibles'
+},
+
+
+{
+    task: 'letter(a;b).number(1;2). Create multible models where all all combinations of letter as a first element and number as the second element.',
+    tip: ' Use a choice rule.',
+    example: '{newAtom(Varable1,Varable2): atom1(Varable1)} :- atom2(Varable2).',
+    solution: 'letter(a;b).number(1;2).{choice(Letter, Number): letter(Letter)} :- number(Number).',
+    output: '16 Models',
+    description: 'repeat concept of choice rules'
+}, 
+
+{
+    task: 'letter(a;b).number(1;2). Make that each number only appears once in each model.',
+    tip: 'you can specify the number of occurrences by = after the {}',
+    example: '{newAtom(Varable1,Varable2): atom1(Varable1)} = 1 :- atom2(Varable2).',
+    solution: 'letter(a;b).number(1;2).{choice(Letter, Number): letter(Letter)} = 1 :- number(Number).',
+    output: '',
+    description: 'introduction to = 1'
+
+}
 
 
 ];
