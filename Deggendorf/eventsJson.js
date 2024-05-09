0a03faf8c00d615b9bacb307f48c567546a7be29
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2024-05-09 21:31:41 +0200

    added burning cinema movies

--- Deggendorf/eventsJson.js
+++ Deggendorf/eventsJson.js
@@ -13,7 +13,6 @@ var eventsJson = [
 
         "descriptionLong": { "de": "Wir werfen einen Blick hinter die Kulissen des E-Sport. Wir haben auch wieder Gäste im Studio, die beiden Brüder Simon Schlegl (E-Sportler), Florian Schlegl (Vereinsgründer) und Simon Bauer (Kommentator). Sie wollen uns einen spannenden Einblick in ihren Alltag geben. Du bist ein leidenschaftlicher E-Sportler, E-Sports Fan oder willst einfach mehr über das Thema erfahren? Dann schalte gerne ein, oder komm zu uns an die THD und tauch mit uns in die spannende Welt des E-Sport ein." },
         "duration": "~2 Stunden",
-        "likes": 5,
         "organizer":
         {
             "name": "Doschauher",
@@ -27,9 +26,7 @@ var eventsJson = [
             "name": "THD, Campus Deggendorf, J007",
             "maps": "https://maps.app.goo.gl/UfmUWQsr5Mu8Ar7N8",
             "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
-        },
-        "sharesGenerated": 5,
-        "sharesClicked": 5
+        }
     },
 
 
@@ -62,7 +59,6 @@ var eventsJson = [
     Nominierungen: 2 Oscars
 ` },
         "duration": "133 Min.",
-        "likes": 5,
         "language": "de",
         "organizer":
         {
@@ -77,9 +73,7 @@ var eventsJson = [
             "name": "THD, Campus Deggendorf, J007",
             "maps": "https://maps.app.goo.gl/UfmUWQsr5Mu8Ar7N8",
             "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
-        },
-        "sharesGenerated": 5,
-        "sharesClicked": 5
+        }
     },
 
 
@@ -92,12 +86,12 @@ var eventsJson = [
         "descriptionShort": "Playing Badminton as university sports with students and friends",
         "price": "Free",
         "registration": "required",
+        "repeat": "weekly",
         "language": "en",
         "images": ["1.jpg", "2.webp", "3.jpg"],
 
         "descriptionLong": { "en": "Join the official Badminton Whatsapp group: https://chat.whatsapp.com/LyGhAonrgY1CiWjjEsHVX2" },
         "duration": "1:30 hours",
-        "likes": 5,
         "organizer":
         {
             "name": "Hendran Manimaran",
@@ -110,9 +104,7 @@ var eventsJson = [
             "name": "Dreifach- Turnhalle Comenius Gymnasium, Jahnstraße 8, 94469 Deggendorf(Zugang Jahnstraße über FOS / BOS) ",
             "maps": "https://maps.app.goo.gl/mYPKEkcVBHG7raa78",
             "description": "Coming from Rewe on the opposite site of Shell go up the stairs to enter."
-        },
-        "sharesGenerated": 5,
-        "sharesClicked": 5
+        }
     },
 
 
@@ -122,7 +114,7 @@ var eventsJson = [
         "timeEntry": "5:44PM",
         "timeStart": "8:00PM",
         "descriptionShort": "CERN, Switzerland visit - United AI 2024",
-        "price": "150$",
+        "price": "150€",
         "registration": "required",
         "language": "en",
         "images": ["1.jpeg", "2.jpg", "3.webp"],
@@ -136,7 +128,6 @@ https://forms.gle/YyXBvFQ8wSPSsqy2A
 
 Don't wait, sign up before May 9th, 2024, 23:59 (UTC) to join us on this incredible journey! 🌟` },
         "duration": "3+ days",
-        "likes": 5,
         "organizer":
         {
             "name": "Tushar Tom",
@@ -149,10 +140,185 @@ Don't wait, sign up before May 9th, 2024, 23:59 (UTC) to join us on this incredi
             "name": "Geneva, hotel.",
             "maps": "https://maps.app.goo.gl/B8JUwQB5pnigjUgN7",
             "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
-        },
-        "sharesGenerated": 5,
-        "sharesClicked": 5
+        }
     },
+
+
+
+    {
+        "name": "Burning Cinema: Das wandelnde Schloss",
+        "date": "Dienstag 07.05.2024",
+        "timeEntry": "19:00",
+        "timeStart": "19:30",
+        "descriptionShort": "Studentenkino: Schoner Anime Film von Studio Ghibli.",
+        "price": "5€",
+        "language": "de",
+        "images": ["1.jpg", "2.jpg", "5.jpg"],
+
+        "descriptionLong": {
+            "de": `
+            Ausnahmsweise am Dienstag | Hörsaal B004
+
+Als sich Sophie unsterblich in den Zauberer Hauro verliebt, wird sie von einer eifersüchtigen Hexe mit einem Fluch belegt und findet sich plötzlich im gebrechlichen Körper einer alten Frau wieder. Beschämt verlässt sie ihre Heimat und findet Zuflucht im geheimnisvollen wandelnden Schloss. Doch der Besitzer des Schlosses, der Zauberer Hauro, schenkt ihr kaum Beachtung. Erst als ein Krieg ausbricht, übernimmt er Verantwortung. Aber wird Sophies Liebe ausreichen, um den Fluch zu lösen und gemeinsam mit Hauro sich selbst und die Welt zu retten?
+
+Phantasie- und humorvolles Märchenabenteuer über ein kleines Mädchen, das von einer Hexe in eine alte Frau verwandelt wird und alles versucht, um den Fluch zu lösen. Ein zeitloses Meisterwerk und ein relativ junger, Oscar-nominierter Meilenstein der Animegeschichte von Studio Ghibli.
+
+    R: Hayao Miyazaki
+    JAP
+    FSK 6
+    Animation
+    119 Min.
+
+    Nominierungen: 1 Oscar
+
+                  ` },
+        "duration": "119 Min.",
+        "language": "de",
+        "organizer":
+        {
+            "name": "Burning Cinema",
+            "website": "https://www.unifilm.de/studentenkinos/Deggendorf",
+            "instagram": "https://www.instagram.com/hochschulkino_deggendorf/",
+            "email": "deggendorf@unifilm.de",
+            "ratings": 4.9
+        },
+        "type": "in person",
+        "location": {
+            "name": "THD, Campus Deggendorf, Hörsaal B004",
+            "maps": "https://maps.app.goo.gl/wrKFLAM7fqAck77b8",
+            "description": "On campus the building with the big white letter B written on it."
+        }
+    },
+
+    {
+        "name": "Burning Cinema: Dungeons & Dragons: Ehre unter Dieben",
+        "date": "Donnerstag 16.05.2024",
+        "timeEntry": "19:00",
+        "timeStart": "19:30",
+        "descriptionShort": "Studentenkino: D&D Rollenspeil Film.",
+        "price": "5€",
+        "language": "de",
+        "images": ["1.jpg", "2.jpg", "3.webp"],
+
+        "descriptionLong": {
+            "de": `
+            Langfinger Edgin bricht in ein episches Abenteuer auf. Unterstützt wird er von einem bunten Team aus Außenseitern mit ungewöhnlichen Talenten: Barbarin Holga, Zauberer Simon, Druidin Doric und Paladin Xenk. Als Gegenspieler hat sich der gerissene Forge in Position gebracht. Das gemeinsame Ziel: eine verlorene Reliquie wiederzubeschaffen. Doch die Dinge gehen gewaltig schief, als sich die Grenzgänger mit den falschen Leuten anlegen. Aber wo keine Ehre, da auch keine Regeln. Und was auch immer sie erwartet, sie werden bereit sein. Vielleicht.
+
+Die vielfältige, fantastische Welt und der spielerische Geist des legendären Rollenspiels kommen in einem umwerfend komischen und actiongeladenen Abenteuer auf die große Leinwand. Angeleitet von John Francis Daley und Jonathan Goldstein bringt ein hochkarätiger Cast echte Filmmagie hervor.
+
+    R: John Francis Daley, Jonathan Goldstein
+    USA, CAN, UK, ISL, IRL, AUS
+    FSK 12
+    Abenteuer
+
+
+                  ` },
+        "duration": "133 Min.",
+        "language": "de",
+        "organizer":
+        {
+            "name": "Burning Cinema",
+            "website": "https://www.unifilm.de/studentenkinos/Deggendorf",
+            "instagram": "https://www.instagram.com/hochschulkino_deggendorf/",
+            "email": "deggendorf@unifilm.de",
+            "ratings": 4.9
+        },
+        "type": "in person",
+        "location": {
+            "name": "THD, Campus Deggendorf, J007",
+            "maps": "https://maps.app.goo.gl/UfmUWQsr5Mu8Ar7N8",
+            "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
+        }
+    },
+    {
+        "name": "Burning Cinema: The Hunger Games: The Ballad of Songbirds & Snakes  [OV]",
+        "date": "Donnerstag 23.05.2024",
+        "timeEntry": "19:00",
+        "timeStart": "19:30",
+        "descriptionShort": "Studentenkino: Die Prequel zu der Hunger Games Fimreihe.",
+        "price": "5€",
+        "language": "de",
+        "images": ["1.jpg", "2.jpg", "3.jpg"],
+
+        "descriptionLong": {
+            "de": `
+            Der junge Coriolanus (Tom Blyth) ist die letzte Hoffnung für seine einst stolze Familie Snow, die in Ungnade gefallen ist. Als er zum Mentor von Lucy Gray (Rachel Zegler), einem Mädchen aus dem verarmten Distrikt 12, erwählt wird, sieht er die Chance, sein Schicksal zu ändern. Denn Lucy Gray zieht das Publikum in ihren Bann. In einem Wettlauf gegen die Zeit und hin- und hergerissen zwischen Gut und Böse kämpft Coriolanus ums Überleben – und es wird sich entscheiden, ob ihn die Geschicke zu einem Singvogel oder zu einer Schlange werden lassen...
+
+Basierend auf der Bestseller-Reihe von Suzanne Collins begeisterten die "Die Tribute von Panem"-Kinofilme weltweit Millionen Fans. Nun erleben wir die Vorgeschichte der Welt von Panem und den Anfang der gefürchteten Hungerspiele: Ein episches Prequel in einem fantastischen visuellen Look.
+
+    R: Francis Lawrence
+    USA, CAN
+    FSK 12
+    Abenteuer
+
+
+                  ` },
+        "duration": "157 Min.",
+        "language": "de",
+        "organizer":
+        {
+            "name": "Burning Cinema",
+            "website": "https://www.unifilm.de/studentenkinos/Deggendorf",
+            "instagram": "https://www.instagram.com/hochschulkino_deggendorf/",
+            "email": "deggendorf@unifilm.de",
+            "ratings": 4.9
+        },
+        "type": "in person",
+        "location": {
+            "name": "THD, Campus Deggendorf, J007",
+            "maps": "https://maps.app.goo.gl/UfmUWQsr5Mu8Ar7N8",
+            "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
+        }
+    },
+    {
+        "name": "Burning Cinema: Wall-E",
+        "date": "Donnerstag 20.06.2024",
+        "timeEntry": "19:00",
+        "timeStart": "19:30",
+        "descriptionShort": "Studentenkino: Der Letzte räumt die Erde auf",
+        "price": "5€",
+        "language": "de",
+        "images": ["1.jpg", "2.jpg", "3.jpg"],
+
+        "descriptionLong": {
+            "de": `
+            700 Jahre in der Zukunft ist unser Planet nicht mehr bewohnbar und total zugemüllt. Die Menschen haben die Erde mittlerweile verlassen und nur noch einige Roboter zum Aufräumen zurückgelassen. Der letzte seiner Art ist WALL-E, der sich mittlerweile, neben seiner Arbeit, mit dem Auflesen von coolen Sammelgegenständen und Ersatzteilen für sich selbst vergnügt. Eines Tages landet ein Raumschiff und bringt EVE – einen wunderschönen weiblichen Roboter, in den sich WALL-E verliebt – EVE ist jedoch nicht zufällig auf der Erde gelandet...
+
+Pixar ist ein Jahr nach RATATOUILLE mit WALL-E erneut ein Meilenstein im Genre des Animationsfilms gelungen. Bemerkenswert ist der weitgehende Verzicht auf Dialoge und die großartige Mimik der Roboter, welche anrührende und tiefgründige Emotionalität erzeugen.
+
+    R: Andrew Stanton
+    USA, JAP
+    FSK 0
+    Animation
+    Preise: 1 Oscar, 1 Golden Globe
+    Nominierungen: 6 Oscars
+                  ` },
+        "duration": "98 Min.",
+        "language": "de",
+        "organizer":
+        {
+            "name": "Burning Cinema",
+            "website": "https://www.unifilm.de/studentenkinos/Deggendorf",
+            "instagram": "https://www.instagram.com/hochschulkino_deggendorf/",
+            "email": "deggendorf@unifilm.de",
+            "ratings": 4.9
+        },
+        "type": "in person",
+        "location": {
+            "name": "THD, Campus Deggendorf, J007",
+            "maps": "https://maps.app.goo.gl/UfmUWQsr5Mu8Ar7N8",
+            "description": "Base floor on the entrince straigt under the stairs through the glass door third room on the left."
+        }
+    },
+
+// Sports
+
+
+
+
+
+
+
 
     
 ];
